e0a7f60a4bf02c4ab540aa307f76e2724d6f6f89
username.ak <sitemakers.3dn.ru@gmail.com>
2015-06-22 14:33:02 +0300

    Add new files

---
+++ gdx-game/src/net/neonet/gamingstudio/neox3d/Main.java
@@ -0,0 +1,43 @@
+package net.neonet.gamingstudio.neox3d;
+
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Main extends Game {
+	Texture texture;
+	SpriteBatch batch;
+
+	@Override
+	public void create() {
+		
+	}
+
+	@Override
+	public void render() {        
+	    super.render();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+}
